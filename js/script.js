32e25a87a4b5faec4751d6c58830b656777fa3a4
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-26 20:54:32 +0300

    feat: created press left and right

--- js/script.js
+++ js/script.js
@@ -1,69 +1,25 @@
+import Player from "./player.js";
+import InputHandler from "./input.js";
+import { drawStatusText } from "./utils.js";
+
 window.addEventListener('load', function(){
-/*   const canvas = document.getElementById('canvas1');
-  const ctx = canvas.getContext('2d'); //default width 300px height 150px
-  canvas.width = 1300;
-  canvas.height = 720; */
+  const loading = this.document.getElementById('loading');
+  loading.style.display = 'none';
+  const canvas = document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-/*   class InputHandler {
-    constructor() {
-      this.keys = [];
-      this.touchY = '';
-      this.touchTreshold = 30;
-      window.addEventListener('keydown', e =>{
-        if((e.key === 'ArrowDown' ||
-           e.key === 'ArrowUp' ||
-           e.key === 'ArrowLeft' ||
-           e.key === 'ArrowRight')
-           && this.keys.indexOf(e.key) === -1){
-          this.keys.push(e.key);
-        }else if (e.key === 'Enter' && gameOver) restartGame(); //new game
-      });
-      window.addEventListener('keyup', e =>{
-        if(e.key === 'ArrowDown' ||
-           e.key === 'ArrowUp' ||
-           e.key === 'ArrowLeft' ||
-           e.key === 'ArrowRight'){
-          this.keys.splice(this.keys.indexOf(e.key, 1));
-        }
-        console.log(e.key, this.keys);
-      });
-      window.addEventListener('touchstart', e => {
-        this.touchY = e.changedTouches[0].pageY;
-      });
-      window.addEventListener('touchmove', e => {
-        const swipeDistance = e.changedTouches[0].pageY - this.touchY;
-        if(swipeDistance < -this.touchTreshold && this.keys.indexOf('swipe up') === -1) this.keys.push('swipe up');
-        else if (swipeDistance > this.touchTreshold && this.keys.indexOf('swipe down') === -1) {
-          this.keys.push('swipe down');
-          if (gameOver) restartGame();
-        } 
+  const player = new Player(canvas.width, canvas.height);
+  const input = new InputHandler();
 
-      });
-      window.addEventListener('touchend', e => {
-        this.keys.splice(this.keys.indexOf('swipe up'), 1);
-        this.keys.splice(this.keys.indexOf('swipe down'), 1);
-      });
-    }
-
-  } */
-/*   class Player {
-    constructor(){
-
-    }
-
-    draw(){
-    }
-
-    update() {
-    }
-  } */
-
-
-
-/* function animate() {
-  //ctx.clearRect(0,0,canvas.width,canvas.height);
-  requestAnimationFrame(animate);
-}
-animate(); */
+  function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    player.update(input.lastKey);
+    player.draw(ctx);
+    drawStatusText(ctx, input, player);
+    requestAnimationFrame(animate);
+  }
+  animate();
 
 });
